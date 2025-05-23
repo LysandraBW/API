07f12fdf609bbd2c147ae78de4989da7cb5859b8
Lysandra Belnavis-Walters <lbelnaviswalters@gmail.com>
2025-05-22 21:46:22 -0400

    Tweaks

--- src/db/Appointment/Label/Update/Update.ts
+++ src/db/Appointment/Label/Update/Update.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 import sql from "mssql";
 import { Data, buildProcedure } from "@/db/Procedure";
-import { appointmentTest } from "@/validate";
+import { appointmentTest, isString } from "@/validate";
 import { isBit, isInteger } from "@/validate";
 import { UNDEFINED_POOL } from "@/constant";
 import { getEmployeePool } from "@/pool";
@@ -11,11 +11,12 @@ export async function ExecuteUpdateLabel(data: Data) {
         const pool = await getEmployeePool(data.sessionID);
         if (!pool)
             throw UNDEFINED_POOL;
+        console.log(data.labelValue, data.labelValue !== "" ? parseInt(data.labelValue) : null)
         await pool.request()
             .input('SessionID', sql.Char(36), data.sessionID)
             .input('AppointmentID', sql.UniqueIdentifier, data.appointmentID)
             .input('LabelID', sql.Int, data.labelID)
-            .input('LabelValue', sql.Bit, data.labelValue)
+            .input('LabelValue', sql.Bit, data.labelValue !== "" ? parseInt(data.labelValue) : null)
             .execute('Appointment.UpdateLabel');
         return true;
     }
@@ -28,7 +29,7 @@ export async function ExecuteUpdateLabel(data: Data) {
 export const TestUpdateLabel = z.object({
     ...appointmentTest,
     labelID: isInteger,
-    labelValue: isBit.optional().transform(v => v === "1" ? 1 : 0)
+    labelValue: isBit.or(z.string().refine(s => s === "")).optional()
 });
 
 export const UpdateLabel = buildProcedure(TestUpdateLabel, ExecuteUpdateLabel);
